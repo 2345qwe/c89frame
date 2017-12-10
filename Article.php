6f6914d5e15098eb30fcb52cdf3594da643b7922
2345qwe <1370797912@qq.com>
2017-12-11 02:26:44 +0800

    Add files via upload

---
+++ Article.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\home\controller;
+//21.创建namespace app\home\controller命名空间
+
+//22.创建Atrticle这个类
+class Article{
+//    23.创建index公有方法
+    public function index(){
+        echo 'article';
+    }
+//    24.创建add公有方法
+    public function add(){
+        echo 'article add';
+    }
+}
